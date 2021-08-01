465b02106b9b7b8ffeb1116e1b37b4fdd171e9b4
jhxu <541468099@qq.com>
2021-08-01 17:05:59 +0800

    完成分析部分

--- titanic/core/data_draw.py
+++ titanic/core/data_draw.py
@@ -1,12 +1,21 @@
+from numpy.lib.npyio import save
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+FILE_PATH = os.path.dirname(__file__)
+SAVE_ROOT = os.path.dirname(FILE_PATH)
 
 def draw(df):
     draw_ratio(df)
     draw_pclass(df)
     draw_sex(df)
     draw_age(df)
+    draw_family(df)
+    draw_ticket(df)
+    draw_cabin(df)
+    draw_embarked(df)
 
 def draw_pclass(df):
     fig1, ax = plt.subplots()
@@ -24,7 +33,7 @@ def draw_pclass(df):
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
     ax.legend()
-    fig1.savefig("./analyze_img/survive_pclass.png")
+    fig1.savefig(os.path.join(SAVE_ROOT, "./analyze_img/02_survive_pclass.png"))
 
 def draw_sex(df):
     # 性别和生还率的关系
@@ -43,21 +52,70 @@ def draw_sex(df):
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
     ax.legend()
-    fig1.savefig("./analyze_img/survive_sex.png")
+    fig1.savefig(os.path.join(SAVE_ROOT, "./analyze_img/03_survive_sex.png"))
 
 def draw_age(df):
     # 年龄和生还率的关系
     fig1, ax = plt.subplots()
     tmp_df = df[["Age", "Survived"]]
     groupby_df = tmp_df.groupby("Survived")
-    print("==========Max age==========\n{}".format(groupby_df.max()))
-    print("==========Min age==========\n{}".format(groupby_df.min()))
-    print("==========Mean age==========\n{}".format(groupby_df.mean()))
+    save_df = groupby_df.min()
+    save_df = save_df.rename(columns={"Age": "Min Age"})
+    save_df["Max Age"] = groupby_df.max()["Age"]
+    save_df["Mean Age"] = groupby_df.mean()["Age"]
+    save_df = save_df.round(2)
+    print("==============Analyze Age==============")
+    print(save_df)
+    save_path = os.path.join(SAVE_ROOT, "./analyze_img/04_survive_age.csv")
+    save_df.to_csv(save_path,sep=',')
 
-    # 兄弟姐妹数量和生还率的关系
+def draw_family(df):
+    tmp_df = df[["Survived", "SibSp", "Parch"]]
+    groupby_df = tmp_df.groupby("Survived")
+    save_df = groupby_df.mean().round(2)
+    save_path = os.path.join(SAVE_ROOT, "./analyze_img/05_survive_family.csv")
+    save_df.to_csv(save_path,sep=',')
+    print("==============Analyze Family==============")
+    print(save_df)
 
-    # 父母、孩子数量和生还率的关系
+def draw_ticket(df):
+    tmp_df = df[["Survived", "Fare"]]
+    groupby_df = tmp_df.groupby("Survived")
+    save_df = groupby_df.mean().round(2)
+    save_path = os.path.join(SAVE_ROOT, "./analyze_img/06_survive_ticket.csv")
+    save_df.to_csv(save_path,sep=',')
+    print("==============Analyze Ticket==============")
+    print(save_df)
 
+def draw_cabin(df):
+    tmp_df = df
+    tmp_df["Cabin"] = tmp_df["Cabin"].apply(lambda s:s[0])
+    count_df = tmp_df.groupby(["Cabin", "Survived"])["Cabin"].count().unstack()
+    count_df = count_df.fillna(0)
+    count_df["Ratio"]  = count_df[1] / (count_df[1] + count_df[0])
+    count_df = count_df.round(2)
+    save_path = os.path.join(SAVE_ROOT, "./analyze_img/07_survive_cabin.csv")
+    count_df.to_csv(save_path,sep=',')
+    print("==============Analyze Cabin==============")
+    print(count_df)
+
+def draw_embarked(df):
+    fig1, ax = plt.subplots()
+    counts = df.groupby(["Embarked", "Survived"])["Embarked"].count()
+    # plot
+    width = 0.35
+    labels = counts.unstack().index.to_list()
+    survive_data = counts.loc[(slice(None), 1)].values
+    not_survive_data = counts.loc[(slice(None), 0)].values
+    x = x = np.arange(len(labels))  # the label locations
+    rects1 = ax.bar(x - width/2, survive_data, width, label='survived')
+    rects2 = ax.bar(x + width/2, not_survive_data, width, label='not survived')
+    ax.set_ylabel('Counts')
+    ax.set_title('Counts by pclass and survive')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    fig1.savefig(os.path.join(SAVE_ROOT, "./analyze_img/08_survive_embarked.png"))
 
 def draw_ratio(df, col="Survived"):
     """Survive的比例图"""
@@ -75,4 +133,4 @@ def draw_ratio(df, col="Survived"):
             shadow=True, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     ax1.set_title("Survived Ratio")
-    fig1.savefig("./analyze_img/survive_ratio.png")
+    fig1.savefig(os.path.join(SAVE_ROOT, "./analyze_img/01_survive_ratio.png"))
--- titanic/core/main.py
+++ titanic/core/main.py
@@ -1,11 +1,14 @@
+import pdb
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from data_draw import draw
 from data_process import nan_process
+import os
 
 def main():
-    data_path = "./data/train.csv"
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(file_path, "..", "data", "train.csv")
     df = pd.read_csv(data_path)
     # draw_ratio(df)
 
