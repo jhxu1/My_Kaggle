e28c2a1b8f72b4cc72c83e51cf591a9c055d68d2
xujiahao <xujiahao@senseauto.com>
2021-08-05 19:31:51 +0800

    delete useless file

--- titanic/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-nums = ["a", "b", "c"]
-data = pd.Series([nums[i] for i in np.random.randint(0, 3, 100)])
-print(data.value_counts())
