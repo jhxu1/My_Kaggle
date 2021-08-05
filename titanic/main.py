96aeb8de30f291f6171dfde7b50c62229b1dd7a5
xujiahao <xujiahao@senseauto.com>
2021-08-05 20:04:27 +0800

    完成初版

--- titanic/main.py
+++ titanic/main.py
@@ -19,8 +19,24 @@ def main():
     x_train, x_test, y_train, y_test = split(df)
     model = Model()
     model.build(x_train, y_train)
+
+    # test
     y_pred = model.predict(x_test)
     evaluate(y_pred, y_test)
+
+    # get result
+    result_path = os.path.join(file_path, "data", "test.csv")
+    save_path = os.path.join(file_path, "data", "test_with_result.csv")
+    df = pd.read_csv(result_path)
+    processd_df = process(df)
+    y_pred = model.predict(processd_df)
+    survived_col = pd.DataFrame(y_pred, columns=["Survived"])
+    survived_col.insert(0, 'PassengerId', df["PassengerId"], allow_duplicates=False)
+    print(survived_col.head())
+    # print(df.head())
+    survived_col.to_csv(save_path, index=False)
+
+
 
 if __name__ == '__main__':
     main()
